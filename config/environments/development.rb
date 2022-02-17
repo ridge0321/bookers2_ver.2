98a6a226eb41e24e215c3a0480c762324a902269
EC2 Default User <ec2-user@ip-172-31-6-219.ec2.internal>
2022-02-17 03:49:13 +0000

    ファイルの設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "9b655c7acd864ccdab51cda0c266ceef.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
 end
